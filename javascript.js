d900eaa9cb443212e60c0e3e6941a21d5e71f7ad
David Forrester <david.o.forrester@gmail.com>
2023-04-04 08:20:28 +1000

    Fundamentals part 1 complete

--- javascript.js
+++ javascript.js
@@ -1,1 +1,21 @@
-console.log("Hello, World!")
+// question 1
+console.log(23 + 97)
+
+// Question 2
+console.log(23 + 97 + 34 + 12 + 64 + 53)
+
+// Question 3
+console.log((4 + 6 + 9) / 77)
+
+// Question 4
+let a = 10
+console.log(a)
+console.log(9 * a)
+let b = 7 * a
+console.log(b)
+
+// Question 5
+let max = 57
+let actual = max -13
+let percentage = actual / max
+console.log(percentage)
