a66614882bde5cba6c48b4fbb8282c933f9391f4
David Forrester <david.o.forrester@gmail.com>
2023-04-06 15:43:50 +1000

    fundamentals 3 practice questions

--- javascript.js
+++ javascript.js
@@ -1,3 +1,34 @@
+//Fundamentals 3
+
+//Question 1
+function add7(number) {
+    return number + 7
+}
+
+console.log(add7(23))
+
+//Question 2
+function multiple(number1, number2) {
+    return number1 * number2
+}
+
+console.log(multiple(5, 2))
+
+// Question 3
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase()
+}
+
+console.log(capitalize("tEsTinG"))
+
+// Question 4
+function lastLetter(string) {
+    return string.charAt(string.length - 1)
+}
+
+console.log(lastLetter("abcde"))
+
+/* Fundamentails 1
 // question 1
 console.log(23 + 97)
 
@@ -19,3 +50,4 @@ let max = 57
 let actual = max -13
 let percentage = actual / max
 console.log(percentage)
+*/
